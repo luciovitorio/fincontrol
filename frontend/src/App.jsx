cd3aa3ac855e36fff2e72d619f45e20aafc74ff7
LucioVitorio <luciovitorio@yahoo.com.br>
2023-05-21 14:43:25 -0300

    Configuração inicial do front com tailwind e heroicons

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { UserIcon } from "@heroicons/react/24/outline";
+
+function App() {
+  return (
+    <div className="bg-purple-400">
+      App.jsx
+      <UserIcon className="h-6 w-6" />
+    </div>
+  );
+}
+
+export default App;
